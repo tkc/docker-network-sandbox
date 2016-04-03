5bdebba4ee04ca86de8ad51eded7e6775d4d2fda
tkc <info@onga.jp>
2016-04-04 01:33:47 +0900

    update

---
+++ Code/laravel/public/index.php
@@ -0,0 +1,4 @@
+<?php
+
+$MYSQL1_PORT = getenv('MYSQL1_PORT');
+print_r($MYSQL1_PORT);
--- docker-init.sh
+++ docker-init.sh
@@ -3,12 +3,31 @@ clear
 docker-compose rm
 docker-compose up -d
 
+echo 'laravel'
+docker exec laravel php -v
+echo 'web1'
 docker exec web1 php -v
+echo 'web2'
 docker exec web2 php -v
+echo 'web3'
 docker exec web3 php -v
-#docker exec web1 composer require vlucas/phpdotenv
 
-docker exec web1 env >> ./Code/.env
-#docker exec web2 env >> ./Code/.env
-#docker exec web3 env >> ./Code/.env
+docker exec laravel env >> ./Code/laravel/.env
+docker exec web1 env >> ./Code/web1/.env
+docker exec web2 env >> ./Code/web2/.env
+docker exec web3 env >> ./Code/web3/.env
+
+function ip() {
+    docker-machine ip
+}
+
+
+echo
+echo "ip address";
+
+iP=`ip`
+echo $iP
+echo
+
+
 #docker exec -ti web1 bash
