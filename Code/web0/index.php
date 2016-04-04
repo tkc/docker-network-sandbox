b281aa2edcc81b92b2a5ba1fd0fd05c890df5de4
tkc <info@onga.jp>
2016-04-04 12:08:54 +0900

    u

--- Code/web0/index.php
+++ Code/web0/index.php
@@ -1,5 +1,9 @@
-test
 
 <?php
+
+ini_set( 'display_errors', 1 );
+
+print_r($_ENV);
 $MYSQL1_PORT = getenv('MYSQL1_PORT');
 print_r($MYSQL1_PORT);
+echo "test";
---
+++ Code/web1/index.php
@@ -0,0 +1,9 @@
+
+<?php
+
+ini_set( 'display_errors', 1 );
+
+print_r($_ENV);
+$MYSQL1_PORT = getenv('MYSQL1_PORT');
+print_r($MYSQL1_PORT);
+echo "test";
